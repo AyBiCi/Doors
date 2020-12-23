27754fc1af6316e3a89182063dd218a9a0f9cd31
AyBiCi <rymcymcym100@gmail.com>
2020-12-23 21:02:56 +0100

    Moved SubcommandExecutor to module.

--- src/main/java/main/DoorsPlugin.java
+++ src/main/java/main/DoorsPlugin.java
@@ -1,5 +1,8 @@
 package main;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.JavaPluginLoader;
@@ -16,6 +19,23 @@ public class DoorsPlugin extends JavaPlugin {
 
     public void onEnable(){
         instance = this;
+        String standardCommandName = "dtp";
+        getCommand(standardCommandName).setExecutor(createSubcommandExecutor(standardCommandName));
+    }
+
+    private SubcommandExecutor createSubcommandExecutor(String commandName){
+        SubcommandExecutor commandExecutor = new SubcommandExecutor(commandName);
+
+        //When player write bad or none subcommand
+        commandExecutor.setDefaultExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+                commandSender.sendMessage("Lista komend - /dtp help.");
+                return false;
+            }
+        });
+
+        return commandExecutor;
     }
 
     //Constuctors for MockBukkit
